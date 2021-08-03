22c46ff76dac24d74b341528313622df4e31629b
AllanZamb <alanzambie@gmail.com>
2021-08-03 13:00:40 -0500

    ok

---
+++ R/DatosCOVID.R
@@ -0,0 +1,18 @@
+
+#' Datos Covid México
+#' @export
+# hello <- function() {
+#   print("Hello, world!")
+# }
+
+DatosCOVID19 <- function() {
+  library(readxl)
+  library(tidyverse)
+  library(RCurl)
+
+  print("Hello, datos!")
+
+}
+
+
+#DatosAbiertosCOVID19::DatosCOVID19()
