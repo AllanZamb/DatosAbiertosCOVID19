69f5609913723b3af06c78b5c0f0bc5944b1f8f0
AllanZamb <alanzambie@gmail.com>
2022-02-14 19:10:49 -0600

    OK

--- R/DatosCOVID.R
+++ R/DatosCOVID.R
@@ -4,7 +4,7 @@
 DatosCOVID19 <- function() {
 
   pacman::p_load(readxl,tidyverse , RCurl,data.table)
-  source("R/funciones_base.R")
+  source("funciones_base.R")
 
   datos_covid <- fread("curl http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip | funzip") %>%
     recodifica_variables(.) %>%
